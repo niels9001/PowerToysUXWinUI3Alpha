24dec5062a8c0cecdebe3355522c62af2af509d7
Niels Laute <niels.laute@live.nl>
2020-02-05 22:08:06 +0100

    Add project files.

---
+++ PowerToysSettingsWinUI3Alpha/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using PowerToysSettingsWinUI3Alpha.Views;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PowerToysSettingsWinUI3Alpha
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NavView.SelectedItem = NavView.MenuItems[0];
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            switch (((NavigationViewItem)args.SelectedItem).Tag.ToString())
+            {
+                case "General": contentFrame.Navigate(typeof(General)); NavView.Header = "General"; break;
+                //case "FancyZones": contentFrame.Navigate(typeof(PowerRename)); NavView.Header = "FancyZones"; break;
+                case "PowerRename": contentFrame.Navigate(typeof(PowerRename)); NavView.Header = "Power Rename"; break;
+                case "Shortcuts": contentFrame.Navigate(typeof(Shortcuts)); NavView.Header = "Shortcuts Guide"; break;
+                case "PowerLauncher": contentFrame.Navigate(typeof(PowerLauncher)); NavView.Header = "Power Launcher"; break;
+            }
+        }
+    }
+}
